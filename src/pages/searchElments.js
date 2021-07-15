5f9ac9910bc200d9ea0e8f45b65cde83152f90c4
mahmoudgmaa <mahmoud.gomaa14@gmail.com>
2021-07-15 19:55:15 +0200

    search done

--- src/pages/searchElments.js
+++ src/pages/searchElments.js
@@ -1,4 +1,4 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const SearchScreenSection = styled.section`
   width: 100vw;
@@ -15,19 +15,16 @@ export const SearchScreenSection = styled.section`
 
 export const ItemsContainer = styled.div`
   width: 60%;
-  /* height: ${({ searched }) => (searched ? "68%" : "55%")}; */
   background: #fff;
   position: relative;
   border-radius: 10px;
   top: ${({ searched }) => (searched ? "0%" : "-40%")};
   transition: 1s ease-in-out;
-  /* &.searched {
-    top: 0%;
-  } */
   @media screen and (max-width: 768px) {
     width: 80%;
   }
 `;
+
 export const SearchWrapper = styled.div`
   width: 100%;
   height: 5.5rem;
@@ -42,7 +39,6 @@ export const TopPartContainer = styled.div`
   justify-content: flex-start;
   align-items: center;
   width: 60%;
-  height: 65%;
   z-index: 5;
   background: #fffaf7;
   @media screen and (max-width: 768px) {
